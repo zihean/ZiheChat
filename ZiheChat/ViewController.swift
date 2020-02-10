89ec2a9af7bcc4e899f3100a53ebcb6233df1812
zihean <1104987206@qq.com>
2020-02-10 09:43:55 +0800

    Initial Commit

---
+++ ZiheChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZiheChat
+//
+//  Created by 安子和 on 2020/2/10.
+//  Copyright © 2020 安子和. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
